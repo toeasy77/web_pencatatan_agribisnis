93f4d3bf6ccfcbe623d6727117a2b53f963266bb
toeasy77 <framtzy77@gmail.com>
2023-06-19 01:26:58 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BendaharaController;
+use App\Http\Controllers\AuthController;
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+//Auth
+Route::get('/', [AuthController::class,'login'])->name('login');
+Route::post('/', [AuthController::class,'authenticated']);
+Route::get('/logout', [AuthController::class,'logout']);
+
+//Dashboard
+Route::get('/dashboard', [BendaharaController::class, 'index'])->middleware('auth');
